f33c06305e7f3d8c59b0fcfbeaf510cdc3838c64
voroba <vvvvorova@gmail.com>
2023-02-08 22:04:42 +0500

    starting

---
+++ src/test/java/ru/vorova/gift/GiftApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vorova.gift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
